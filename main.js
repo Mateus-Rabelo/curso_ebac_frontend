e6017cff925b21c11c692b7854c4f6f659d66a8f
Mateus Rabelo <rabelo.mateus4442@gamil.com>
2024-05-23 10:49:10 -0300

    limpeza da main

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('#produtos .produto button');
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('Produto adicionado ao carrinho!');
-        });
-    });
-});
