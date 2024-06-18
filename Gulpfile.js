c4ed25d291cc688dcf074d727483b8cd6419a824
Mateus Rabelo <rabelo.mateus4442@gamil.com>
2024-06-18 09:36:55 -0300

    jason

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,9 @@
-// Importar os módulos necessários
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
-const rename = require('gulp-rename');
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import cleanCSS from 'gulp-clean-css';
+import uglify from 'gulp-uglify';
+import imagemin from 'gulp-imagemin';
+import rename from 'gulp-rename';
 
 // Caminhos dos arquivos
 const paths = {
@@ -23,44 +22,39 @@ const paths = {
 };
 
 // Tarefa de compilação do SASS
-function styles() {
+export function styles() {
     return gulp.src(paths.styles.src)
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest(paths.styles.dest));
-}
+    }
 
-// Tarefa de compressão de imagens
-function images() {
+  // Tarefa de compressão de imagens
+    export function images() {
     return gulp.src(paths.images.src)
     .pipe(imagemin())
     .pipe(gulp.dest(paths.images.dest));
-}
+    }
 
-// Tarefa de compressão de código JavaScript
-function scripts() {
+  // Tarefa de compressão de código JavaScript
+    export function scripts() {
     return gulp.src(paths.scripts.src)
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest(paths.scripts.dest));
-}
-
-// Tarefa para assistir alterações nos arquivos
-function watch() {
+  }
+  
+  // Tarefa para assistir alterações nos arquivos
+  export function watch() {
     gulp.watch(paths.styles.src, styles);
     gulp.watch(paths.scripts.src, scripts);
     gulp.watch(paths.images.src, images);
-}
-
-// Definir tarefas padrão e nomeadas
-gulp.task('styles', styles);
-gulp.task('images', images);
-gulp.task('scripts', scripts);
-gulp.task('watch', watch);
-
-// Definir a tarefa build como uma série de tarefas paralelas
-gulp.task('build', gulp.series(gulp.parallel(styles, images, scripts)));
-
-// Definir a tarefa default para executar build e watch
-gulp.task('default', gulp.series('build', 'watch'));
+  }
+  
+  // Definir tarefas padrão e nomeadas
+  const build = gulp.series(gulp.parallel(styles, images, scripts));
+  const defaultTask = gulp.series(build, watch);
+  
+  // Exportar tarefas
+  export { build, defaultTask as default };
