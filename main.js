12be92846bad5e388a2b9533bbb5c26bbfc2d56a
Mateus Rabelo <rabelo.mateus4442@gamil.com>
2024-07-13 14:33:26 -0300

    Restauração do codigo perdido

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-// Definindo a classe Aluno
-class Aluno {
-    nome: string;
-    nota: number;
-    
-    constructor(nome: string, nota: number) {
-        this.nome = nome;
-        this.nota = nota;
-    }
-    }
-
-// Função para filtrar alunos com nota maior ou igual a 6
-    function filtrar
+    // Array de objetos com nome e nota dos alunos
+    const alunos = [
+        { nome: 'Ana', nota: 8 },
+        { nome: 'Carlos', nota: 5 },
+        { nome: 'Bianca', nota: 7 },
+        { nome: 'Daniel', nota: 4 },
+        { nome: 'Eduarda', nota: 6 },
+        { nome: 'Felipe', nota: 9 }
+        ];
+        
+    // Função para filtrar alunos com nota maior ou igual a 6
+        function filtrarAprovados(alunos) {
+        return alunos.filter(aluno => aluno.nota >= 6);
+        }
+        
+    // Usando a função para obter os alunos aprovados
+        const alunosAprovados = filtrarAprovados(alunos);
+        
+    // Mostrando os alunos aprovados
+        console.log(alunosAprovados);
